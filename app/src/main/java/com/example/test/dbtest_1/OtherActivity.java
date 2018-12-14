8a71a6c772704c1bf87cb588225aacd4155084a6
T <rameshkumar.t@dxc.com>
2018-12-14 11:32:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test/dbtest_1/OtherActivity.java
@@ -0,0 +1,13 @@
+package com.example.test.dbtest_1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class OtherActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
